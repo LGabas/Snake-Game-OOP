18dc9a39e1985aa5f69441e53c282538e90ca406
Lucas Gabas <lgabas95@gmail.com>
2023-05-31 22:48:00 -0300

    Actualizacion

--- scoreboard.py
+++ scoreboard.py
@@ -8,6 +8,7 @@ class Score(Turtle):
     def __init__(self):
         super().__init__()
         self.score = 0
+        self.high_score = 0
         self.goto(x=0, y=270)
         self.color('white')
         self.hideturtle()
@@ -19,9 +20,11 @@ class Score(Turtle):
         self.undo()
         self.write(f'Score = {self.score}', True, align=ALIGN, font=FONT)
 
-    def game_over(self):
-        self.goto(x=0, y=0)
-        self.write(f'Game Over', True, align=ALIGN, font=FONT)
+    def reset(self):
+        if self.score > self.high_score:
+            self.high_score = self.score
+        self.score = 0
+
 
 
 
